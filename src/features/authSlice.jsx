d3076a237f8818f8c38f8bc8fe6aadda57a79167
MehmetFK <mehmetfkuruldak@gmail.com>
2022-11-22 14:46:10 +0100

    reducer added

---
+++ src/features/authSlice.jsx
@@ -0,0 +1,49 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const authSlice = createSlice({
+    name: 'auth',
+
+    initialState: {
+        currentUser: null,
+        loading: false,
+        error: false,
+        isAdmin: false,
+        token: null,
+    },
+    reducers: {
+        fetchStart: (state) => {
+            state.loading = true;
+            state.error = false;
+        },
+        loginSuccess: (state, { payload }) => {
+            state.loading = false;
+            state.currentUser = payload?.user?.username;
+            state.isAdmin = payload?.user?.is_superuser;
+            state.token = payload?.key;
+        },
+        logoutSuccess: (state, { payload }) => {
+            state.loading = false;
+            state.currentUser = null;
+            state.token = null;
+        },
+        registerSuccess: (state, { payload }) => {
+            state.loading = false;
+            state.currentUser = payload?.username;
+            state.isAdmin = payload?.user?.is_superuser;
+            state.token = payload?.token;
+        },
+        fetchFail: (state) => {
+            state.loading = false;
+            state.error = true;
+        },
+    },
+});
+
+export const {
+    fetchStart,
+    loginSuccess,
+    logoutSuccess,
+    registerSuccess,
+    fetchFail,
+} = authSlice.actions;
+export default authSlice.reducer;
